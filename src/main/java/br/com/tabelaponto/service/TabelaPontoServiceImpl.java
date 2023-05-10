c1516062034d93d44fa9c46d9d08f311429ee94c
Marcos Paulo <marcos.paulo.mpsb99@gmail.com>
2023-05-10 03:26:37 -0300

    fix bugs

--- src/main/java/br/com/tabelaponto/service/TabelaPontoServiceImpl.java
+++ src/main/java/br/com/tabelaponto/service/TabelaPontoServiceImpl.java
@@ -1,5 +1,7 @@
 package br.com.tabelaponto.service;
 
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -10,134 +12,205 @@ public class TabelaPontoServiceImpl implements TabelaPontoService {
 
 	@Override
 	public Periodo calcularMarcacoes(List<Horario> horarios, List<Horario> horariosTrabalhados) {
-		
+
 		Periodo periodo = new Periodo();
-		
+
 		periodo.setAtraso(calcularAtrasos(horarios, horariosTrabalhados));
 		periodo.setExtra(calcularExtras(horarios, horariosTrabalhados));
 		return periodo;
 	}
-	
+
 	private List<Horario> calcularAtrasos(List<Horario> horarios, List<Horario> horariosTrabalhados) {
 		List<Horario> atrasos = new ArrayList<>();
 
-	    for (Horario intervaloHorario : horarios) {
-	        boolean horarioTrabalhadoEncontrado = false;
+		for (Horario horario : horarios) {
+			boolean horarioTrabalhadoEncontrado = false;
 
-	        for (Horario intervaloMarcacao : horariosTrabalhados) {
-	        	
- 	            if ((intervaloMarcacao.getFim().isAfter(intervaloHorario.getInicio()) ||
- 	            		intervaloMarcacao.getFim().equals(intervaloHorario.getInicio())) &&
- 	            		(intervaloHorario.getFim().isAfter(intervaloMarcacao.getInicio()) ||
- 	            				intervaloHorario.getFim().equals(intervaloMarcacao.getInicio()))) {
-	            	
-	            	if (intervaloHorario.getInicio().isBefore(intervaloMarcacao.getInicio())) {
+			for (Horario horarioTrabalhado : horariosTrabalhados) {
 
-	                    atrasos.add(new Horario(intervaloHorario.getInicio(), intervaloMarcacao.getInicio()));
-		                if (intervaloHorario.getFim().isAfter(intervaloMarcacao.getFim())) {
+				if (isAfterMeioDia(horario)) {
+					if (isAfterMeioDia(horarioTrabalhado)) {
+						if ((horarioTrabalhado.getInicio().isAfter(horario.getFim())
+								|| horarioTrabalhado.getInicio().equals(horario.getFim()))
+								&& (horario.getInicio().isAfter(horarioTrabalhado.getFim())
+										|| horario.getInicio().equals(horarioTrabalhado.getFim()))) {
 
-		                    atrasos.add(new Horario(intervaloMarcacao.getFim(), intervaloHorario.getFim()));
-		                }
-	                } else if (intervaloHorario.getFim().isAfter(intervaloMarcacao.getFim()) && 
-	                		intervaloHorario.getInicio().isBefore(intervaloMarcacao.getFim()) &&
-	                		!intervaloHorario.getInicio().equals(intervaloMarcacao.getFim())) {
+							if (horario.getInicio().isBefore(horarioTrabalhado.getInicio())) {
+								atrasos.add(new Horario(horario.getInicio(), horarioTrabalhado.getInicio()));
+							}
+							if (horario.getFim().isAfter(horarioTrabalhado.getFim())) {
+								atrasos.add(new Horario(horarioTrabalhado.getFim(), horario.getFim()));
+							}
+							horarioTrabalhadoEncontrado = true;
 
-		                    atrasos.add(new Horario(intervaloMarcacao.getFim(), intervaloHorario.getFim()));
-		                }
-	            	horarioTrabalhadoEncontrado = true;
-	            } else if ((intervaloMarcacao.getInicio().isBefore(intervaloHorario.getInicio()) &&
- 	            		intervaloMarcacao.getFim().equals(intervaloHorario.getFim())) &&
- 	            		(intervaloHorario.getFim().isAfter(intervaloMarcacao.getInicio()) ||
- 	            				intervaloHorario.getFim().equals(intervaloMarcacao.getInicio()))) {
-	            	horarioTrabalhadoEncontrado = true;
-	            }
-	        }
+						} else if (horario.getInicio().equals(horarioTrabalhado.getInicio())
+								&& horario.getFim().equals(horarioTrabalhado.getFim())) {
 
-	        if (!horarioTrabalhadoEncontrado) {
-	            atrasos.add(intervaloHorario);
-	        }
-	        
-	    }
-	    
+							horarioTrabalhadoEncontrado = true;
+
+						}
+
+					} else {
+						if (horarioTrabalhado.getInicio().isBefore(horario.getFim())) {
+							atrasos.add(new Horario(horario.getInicio(), horarioTrabalhado.getInicio()));
+						}
+
+					}
+
+				} else if (isAfterMeioDia(horarioTrabalhado)) {
+
+					if (horarioTrabalhado.getInicio().isAfter(horario.getFim())) {
+						if(horarioTrabalhado.getFim().equals(horario.getInicio())) {
+							atrasos.add(new Horario(horarioTrabalhado.getInicio(), horario.getInicio()));
+						}
+						atrasos.add(new Horario(horario.getInicio(), horarioTrabalhado.getFim()));
+						horarioTrabalhadoEncontrado = true;
+					}
+					if (horarioTrabalhado.getFim().isAfter(horario.getInicio())) {
+						if (horarioTrabalhado.getInicio().equals(horario.getFim())) {
+							atrasos.add(new Horario(horarioTrabalhado.getFim(), horario.getFim()));
+						}
+						atrasos.add(new Horario(horario.getInicio(), horarioTrabalhado.getFim()));
+						horarioTrabalhadoEncontrado = true;
+					}
+					if(horarioTrabalhado.getFim().equals(horario.getInicio()) && horarioTrabalhado.getInicio().equals(horario.getFim())) {
+						horarioTrabalhadoEncontrado = false;
+					}
+					
+
+				} else {
+					if ((horarioTrabalhado.getFim().isAfter(horario.getInicio())
+							|| horarioTrabalhado.getFim().equals(horario.getInicio()))
+							&& (horario.getFim().isAfter(horarioTrabalhado.getInicio())
+									|| horario.getFim().equals(horarioTrabalhado.getInicio()))) {
+
+						if (horario.getInicio().isBefore(horarioTrabalhado.getInicio())) {
+							atrasos.add(new Horario(horario.getInicio(), horarioTrabalhado.getInicio()));
+						}
+						if (horario.getFim().isAfter(horarioTrabalhado.getFim())) {
+							atrasos.add(new Horario(horarioTrabalhado.getFim(), horario.getFim()));
+						}
+						horarioTrabalhadoEncontrado = true;
+
+					} else if (horario.getInicio().equals(horarioTrabalhado.getInicio())
+							&& horario.getFim().equals(horarioTrabalhado.getFim())) {
+						horarioTrabalhadoEncontrado = true;
+					}
+				}
+
+			}
+
+			if (!horarioTrabalhadoEncontrado) {
+				atrasos.add(horario);
+			}
+		}
+
 		List<Horario> atrasosIteradores = new ArrayList<>(atrasos);
 		List<Horario> atrasosCorrigidos = new ArrayList<>(atrasos);
-		
+
 		int i = 0;
-        for (Horario atrasoIterado : atrasosIteradores) {
-	        for(Horario atraso : atrasos) {
-	        	if(atraso.getFim().isBefore(atrasoIterado.getFim()) && atrasoIterado.getInicio().isBefore(atraso.getFim())) {
-	        		try {
-	        			atrasosCorrigidos.remove(i);
-	        			atrasosCorrigidos.remove(i++);
-	        		} catch (IndexOutOfBoundsException ignored) {
-	        			
-	        		}
-	        		if(!atrasosIteradores.get(atrasosIteradores.size() - 1).getFim().isAfter(atraso.getFim())) {
-	        			atrasosCorrigidos.add(new Horario(atrasoIterado.getInicio(), atraso.getFim()));
-	        		}
-	        		i--;
-	        	} 
-	        	if(atraso.getInicio().isBefore(atrasoIterado.getInicio()) && 
-	        			atraso.getFim().isBefore(atrasoIterado.getFim()) &&
-	        			atrasoIterado.getInicio().isBefore(atraso.getFim())) {
-	        		atrasosCorrigidos.add(new Horario(atrasoIterado.getInicio(), atraso.getFim()));
-	        	}
-	        } i++;
-        }
-        return atrasosCorrigidos;
+		for (Horario atrasoIterado : atrasosIteradores) {
+			for (Horario atraso : atrasos) {
+				if (atraso.getFim().isBefore(atrasoIterado.getFim())
+						&& atrasoIterado.getInicio().isBefore(atraso.getFim())) {
+					try {
+						atrasosCorrigidos.remove(i);
+						atrasosCorrigidos.remove(i);
+					} catch (IndexOutOfBoundsException ignored) {
+
+					}
+					if (!atrasosIteradores.get(atrasosIteradores.size() - 1).getFim().isAfter(atraso.getFim())) {
+						atrasosCorrigidos.add(new Horario(atrasoIterado.getInicio(), atraso.getFim()));
+					}
+					i--;
+				}
+			}
+			i++;
+		}
+		return atrasosCorrigidos;
 	}
 
 	private List<Horario> calcularExtras(List<Horario> horarios, List<Horario> horariosTrabalhados) {
 		List<Horario> horasExtras = new ArrayList<>();
 
-	    for (Horario intervaloHorario : horarios) {
-	        boolean horarioTrabalhadoEncontrado = false;
+		for (Horario horarioTrabalhado : horariosTrabalhados) {
+			boolean horarioEncontrado = false;
 
-	        for (Horario intervaloMarcacao : horariosTrabalhados) {
-	        	
-	            if ((intervaloMarcacao.getInicio().isBefore(intervaloHorario.getFim()) || intervaloMarcacao.getInicio().equals(intervaloHorario.getFim())) && (intervaloHorario.getInicio().isBefore(intervaloMarcacao.getFim()) || intervaloHorario.getInicio().equals(intervaloMarcacao.getFim()))) {
+			for (Horario horario : horarios) {
+				if (isAfterMeioDia(horario)) {
+					if (isAfterMeioDia(horarioTrabalhado)) {
 
-	                if (intervaloHorario.getInicio().isAfter(intervaloMarcacao.getInicio())) {
+						if ((horario.getInicio().isAfter(horarioTrabalhado.getFim())
+								|| horario.getInicio().equals(horarioTrabalhado.getFim()))
+								&& (horarioTrabalhado.getInicio().isAfter(horario.getFim())
+										|| horarioTrabalhado.getInicio().equals(horario.getFim()))) {
 
-	                	horasExtras.add(new Horario(intervaloMarcacao.getInicio(), intervaloHorario.getInicio()));
-	                }
+							if (horarioTrabalhado.getInicio().isAfter(horario.getInicio())) {
+								horasExtras.add(new Horario(horario.getInicio(), horarioTrabalhado.getInicio()));
+							}
+							if (horarioTrabalhado.getFim().isAfter(horario.getFim())) {
+								horasExtras.add(new Horario(horario.getFim(), horarioTrabalhado.getFim()));
+							}
+							horarioEncontrado = true;
 
-	                if (intervaloHorario.getFim().isBefore(intervaloMarcacao.getFim())) {
+						}
 
-	                	horasExtras.add(new Horario(intervaloHorario.getFim(), intervaloMarcacao.getFim()));
-	                }
-	                horarioTrabalhadoEncontrado = true;
-	            } else if(intervaloMarcacao.getInicio().isBefore(intervaloHorario.getInicio()) && intervaloMarcacao.getFim().isBefore(intervaloHorario.getInicio())) {
-	            	horasExtras.add(new Horario(intervaloMarcacao.getInicio(), intervaloMarcacao.getFim()));
-	            }
-	        }
+					} else if (horarioTrabalhado.getFim().isBefore(horario.getFim())) {
 
-	        if (!horarioTrabalhadoEncontrado) {
-	        	horasExtras.add(intervaloHorario);
-	        }
-	        
-	    }
-	    return horasExtras;
-//	    List<Horario> extrasIteradores = new ArrayList<>(horasExtras);
-//		List<Horario> extrasCorrigidos = new ArrayList<>(horasExtras);
-//		
-//		int i = 0;
-//        for (Horario intervaloMarcacao : extrasIteradores) {
-//	        for(Horario atraso : horasExtras) {
-//	        	if(atraso.getFim().isBefore(intervaloMarcacao.getFim()) && intervaloMarcacao.getInicio().isBefore(atraso.getFim())) {
-//	        		try {
-//	        			extrasCorrigidos.remove(i);
-//	        			extrasCorrigidos.remove(i++);
-//	        		} catch (IndexOutOfBoundsException ignored) {
-//	        			
-//	        		}
-//	        		if(!extrasIteradores.get(extrasIteradores.size() - 1).getFim().isAfter(atraso.getFim())) {
-//	        			extrasCorrigidos.add(new Horario(intervaloMarcacao.getInicio(), atraso.getFim()));
-//	        		}
-//	        		i--;
-//	        	}
-//	        } i++;
-//        }
-//        return extrasCorrigidos;
-    }
+						horasExtras.add(new Horario(horario.getFim(), horarioTrabalhado.getFim()));
+						horarioEncontrado = true;
+					} else if (horarioTrabalhado.getInicio().isAfter(horario.getFim())) {
+						if (horarioTrabalhado.getFim().isAfter(horario.getInicio())) {
+							horasExtras.add(new Horario(horarioTrabalhado.getInicio(), horario.getInicio()));
+							horarioEncontrado = true;
+						}
+					}
+
+				} else if (isAfterMeioDia(horarioTrabalhado)) {
+
+					if (horario.getFim().isBefore(horarioTrabalhado.getInicio())) {
+						horasExtras.add(new Horario(horarioTrabalhado.getInicio(), horario.getFim()));
+						horarioEncontrado = true;
+					}
+					if (horario.getInicio().isBefore(horarioTrabalhado.getFim())) {
+						horasExtras.add(new Horario(horarioTrabalhado.getInicio(), horario.getInicio()));
+						horarioEncontrado = true;
+					}
+					
+
+				} else {
+
+					if ((horario.getFim().isAfter(horarioTrabalhado.getInicio())
+							|| horario.getFim().equals(horarioTrabalhado.getInicio()))
+							&& (horarioTrabalhado.getFim().isAfter(horario.getInicio())
+									|| horarioTrabalhado.getFim().equals(horario.getInicio()))) {
+
+						if (horarioTrabalhado.getInicio().isAfter(horario.getInicio())) {
+							horasExtras.add(new Horario(horario.getInicio(), horarioTrabalhado.getInicio()));
+						}
+						if (horarioTrabalhado.getFim().isAfter(horario.getFim())) {
+							horasExtras.add(new Horario(horario.getFim(), horarioTrabalhado.getFim()));
+						}
+						horarioEncontrado = true;
+					}
+
+				}
+				if (horarioTrabalhado.getInicio().equals(horario.getInicio())
+						&& horarioTrabalhado.getFim().equals(horario.getFim())) {
+					horarioEncontrado = true;
+				}
+			}
+
+			if (!horarioEncontrado) {
+				horasExtras.add(horarioTrabalhado);
+			}
+		}
+		return horasExtras;
+
+	}
+
+	private Boolean isAfterMeioDia(Horario horario) {
+		LocalTime marcaMeioDia = LocalTime.parse("12:00", DateTimeFormatter.ofPattern("HH:mm"));
+		return horario.getInicio().isAfter(marcaMeioDia) && horario.getFim().isBefore(marcaMeioDia);
+	}
 }
